dd08c3e6d9ffe563f53111f7ad43a9a918034f94
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-05-19 13:18:44 +0700

    feat: modul 11

--- lib/main.dart
+++ lib/main.dart
@@ -7,39 +7,20 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Praktikum App'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,68 +29,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    final List<String> data = <String>[
+      "Wahyu",
+      "Bagja",
+      "Daffa",
+      "Faza",
+      "Tius",
+    ];
+    return DefaultTabController(
+      length: 5,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(widget.title),
+          ),
+          bottom: const TabBar(
+            tabs: <Widget>[
+              Tab(
+                child: Text("Home", style: TextStyle(fontSize: 7)),
+                icon: Icon(Icons.home_filled, size: 25),
+              ),
+              Tab(
+                child: Text("Account", style: TextStyle(fontSize: 7)),
+                icon: Icon(Icons.business_center, size: 25),
+              ),
+              Tab(
+                child: Text("Alarm", style: TextStyle(fontSize: 7)),
+                icon: Icon(Icons.connect_without_contact, size: 25),
+              ),
+              Tab(
+                child: Text("Power Off", style: TextStyle(fontSize: 7)),
+                icon: Icon(Icons.power_off, size: 25),
+              ),
+              Tab(
+                child: Text("Account", style: TextStyle(fontSize: 7)),
+                icon: Icon(Icons.account_box_outlined, size: 25),
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            GridView.count(
+              primary: false,
+              padding: const EdgeInsets.all(20),
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              crossAxisCount: 2,
+              childAspectRatio: (1 / 1),
+              children: <Widget>[
+                Container(
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text("Test Praktikum"),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(1),
+                  color: Colors.brown,
+                  child: const Text('grid 1'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  color: Colors.blue,
+                  child: const Text('grid 2'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  color: Colors.green,
+                  child: const Text('grid 3'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  color: Colors.black,
+                  child: const Text('grid 4'),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(5),
+                  color: Colors.red,
+                  child: const Text('grid 5'),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int idx) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue[100],
+                    border: Border.all(
+                      color: Colors.black,
+                      width: .5,
+                    ),
+                  ),
+                  height: 50,
+                  child: Center(child: Text(data[idx])),
+                );
+              },
+            ),
+            const Center(
+              child: Text("Alarm Page"),
+            ),
+            const Center(
+              child: Text("Power Off Page"),
+            ),
+            ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int idx) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: Colors.lightBlue[100],
+                    border: Border.all(
+                      color: Colors.black,
+                      width: .5,
+                    ),
+                  ),
+                  height: 50,
+                  child: Center(child: Text(data[idx])),
+                );
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
