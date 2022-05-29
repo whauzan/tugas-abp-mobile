afdfc310cec630d74407071fb1b00dab6ccf56ad
Wahyu Hauzan Rafi <57913209+whauzan@users.noreply.github.com>
2022-05-30 02:34:18 +0700

    feat: tpmodul13

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:tugas_abp_mobile/main.dart';
+
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MyHomePage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
